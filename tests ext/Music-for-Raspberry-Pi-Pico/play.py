7bd6e43307e63eac2b5f501336f4081346e9f5a9
pascgu <github@picibi.fr>
2024-12-05 12:53:58 +0100

    lesdson 20 et music for Pico

---
+++ tests ext/Music-for-Raspberry-Pi-Pico/play.py
@@ -0,0 +1,84 @@
+# source : https://github.com/twisst/Music-for-Raspberry-Pi-Pico/tree/main
+
+from machine import Pin, PWM
+from utime import sleep
+
+from melodies import *  # import melodies.py
+from notes import *     # import notes.py
+
+buzzer = PWM(Pin(20))   # pin where buzzer is connected
+button = Pin(17, Pin.IN, Pin.PULL_UP)  # pin where you may connect a button
+
+track = 6      # choose track here (see the list in melodies.py)
+volume = 2000   # set volume to a value between 0 and 1000
+
+
+# functions to play the melodies
+
+def playtone(frequency):
+    buzzer.duty_u16(volume)
+    buzzer.freq(frequency)
+
+def be_quiet():
+    buzzer.duty_u16(0)  # turns sound off
+
+def duration(tempo, t):
+    
+    # calculate the duration of a whole note in milliseconds (60s/tempo)*4 beats
+    wholenote = (60000 / tempo) * 4
+    
+    # calculate the duration of the current note
+    # (we need an integer without decimals, hence the // instead of /)
+    if t > 0:
+      noteDuration = wholenote // t
+    elif (t < 0):
+      # dotted notes are represented with negative durations
+      noteDuration = wholenote // abs(t)
+      noteDuration *= 1.5 # increase their duration by a half
+    
+    return noteDuration
+
+def playsong(mysong):
+
+    try:
+        
+        print(mysong[0]) # print title of the song to the shell 
+        tempo = mysong[1] # get the tempo for this song from the melodies list 
+
+        # iterate over the notes of the melody. 
+        # The array is twice the number of notes (notes + durations)
+        for thisNote in range(2, len(mysong), 2):
+            
+            noteduration = duration(tempo, int(mysong[thisNote+1]))
+            
+            if (mysong[thisNote] == "REST"):
+                be_quiet()
+            else:
+                playtone(notes[mysong[thisNote]])
+            
+            sleep(noteduration*0.9/1000) # we only play the note for 90% of the duration...
+            be_quiet()
+            sleep(noteduration*0.1/1000) # ... and leave 10% as a pause between notes
+        
+            if button.value()==0: # stop playing this track if button is pushed
+                return
+            
+    except: # make sure the buzzer stops making noise when something goes wrong or when the script is stopped
+        be_quiet()
+        
+
+playsong(melody[track])  # actually start playing the melody
+
+
+# the next part allows you to add a button to switch melodies
+
+while True: # constantly keep checking if the button is being pressed
+    if button.value()==0: # if it is...
+        while button.value()==0: # ...first wait for the button to be unpressed
+            sleep(0.1)
+        track+=1 # ... and then play the next track
+        if track > len(melody):
+            track = 0
+        print(track)
+        playsong(melody[track])  # start playing the melody
+    sleep(0.1)
---
+++ tuto paul McWhorter/lesson20-DHT11 temp and humidity sensor.py
@@ -0,0 +1,27 @@
+''' Lesson 20 : DHT 11 temperature and humidity sensor
+'''
+
+from machine import Pin
+import utime as time
+from dht import DHT11
+
+# Dans la vidéo, Ici on va mettre la Pin en OUT alors qu'on va lire dessus, pourquoi ? Parce qu'on veut utiliser un pull down resistor.
+#   c'est contre-intuitif mais c'est volontaire et géré correctement ensuite par la lib DHT11.
+# myPin=Pin(16, Pin.OUT, Pin.PULL_DOWN) # Pin.PULL_DOWN indique que l'on veut que cette pin soit relié à GND et une résistance entre 50k-80k Ohm
+# En fait ça fait une erreur et pour corriger il faut remettre Pin.IN, va savoir...
+myPin=Pin(16, Pin.IN, Pin.PULL_DOWN) # Pin.PULL_DOWN indique que l'on veut que cette pin soit relié à GND et une résistance entre 50k-80k Ohm
+sensor=DHT11(myPin)
+
+print("My sensor values:")
+while True:
+    try:
+        sensor.measure() # je pense qu'on déclenche la mesure du capteur avant de récupérer les valeurs mesurées
+        tempC=sensor.temperature()
+        hum=sensor.humidity()
+        #print(f"temp={tempC}°C humidity={hum}%")
+        print(f"\r temp={tempC}°C humidity={hum}%", end='') # pour ré-afficher sur la même ligne
+        time.sleep(1)
+    except KeyboardInterrupt: # Ctrl-C
+        break
+print('') # pour qu'un retour à la ligne soit fait
+print('prog end')
